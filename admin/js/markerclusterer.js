6e85731ebdd98dd9df058901ae7ba6328544efc2
rahulchauhan2006 <rc9553619@gmail.com>
2025-06-11 04:11:45 -0700

    Add files via upload

---
+++ admin/js/markerclusterer.js
@@ -0,0 +1,5 @@
+
+console.warn('This version of the marker clusterer is no longer in service.\n\
+  We recommend updating your code to use @googlemaps/markerclusterer instead.\n\
+  More information is available at https://github.com/googlemaps/js-markerclusterer\n\
+  and https://developers.google.com/maps/documentation/javascript/marker-clustering.');
